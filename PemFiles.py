e9d9a9e2a9f3a52b45f563df89b9c6e142567a5b
Teodora Lazarevic <lt220136d@student.etf.bg.ac.rs>
2026-06-19 16:02:45 +0200

    Modified format of PEM file

--- PemFiles.py
+++ PemFiles.py
@@ -13,7 +13,7 @@ def import_public_key(pem_file_name: str):
     with open(pem_file_name, "r") as f:
         lines = f.readlines()
 
-    base_64_lines = [line.strip() for line in lines if "BEGIN USER IDENTITY" not in line and "END USER IDENTITY" not in line]
+    base_64_lines = [line.strip() for line in lines if "BEGIN PUBLIC KEY" not in line and "END PUBLIC KEY" not in line]
 
     base_64_string = "".join(base_64_lines)
     json_bytes = base64.b64decode(base_64_string)
@@ -37,9 +37,9 @@ def export_public_key(public_key: RSAPublicKey, name: str, email: str, timestamp
     base_64_string = base64.b64encode(json_bytes).decode('utf-8')
 
     with open(name + '-' + email + ".pem", "w") as f:
-        f.write("-----BEGIN USER IDENTITY-----\n")
+        f.write("-----BEGIN PUBLIC KEY-----\n")
         f.write(base_64_string)
-        f.write("-----END USER IDENTITY-----\n")
+        f.write("-----END PUBLIC KEY-----\n")
 
 def import_key_pair(pem_file_name: str, password: str) -> None:
     with open(pem_file_name, "r") as f:
