c6ebe0d7a1610f02b59b3f3ae31a0b6653ec2f56
Teodora Lazarevic <lt220136d@student.etf.bg.ac.rs>
2026-06-19 16:44:19 +0200

    Implemented signature verifying. Added base64 encoding for JSON serialization.

--- Authentication.py
+++ Authentication.py
@@ -1,6 +1,6 @@
+import base64
 import json
 from time import time
-import hashlib
 
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
@@ -8,18 +8,55 @@ from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPubl
 
 
 def sign(message: bytes, private_key: RSAPrivateKey) -> bytes:
-    timestamp = int(time()).to_bytes(8)
+    timestamp = int(time()).to_bytes(8, byteorder='big')
+
+    signature = private_key.sign(timestamp + message,padding.PKCS1v15(), hashes.SHA1())
 
     signature_and_message = {
-        "timestamp": timestamp,
+        "timestamp": base64.b64encode(timestamp).decode('utf-8'),
         "sender_key_id": private_key.public_key().public_numbers().n % (2 ** 64),
-        "leading_two_octets": timestamp[:2], # vodeca dva okteta (bajta) su prva 2 bajta 8-bajtnog timestamp-a
-        "message_digest": private_key.sign(timestamp + message, padding.PKCS1v15(), hashes.SHA1()), # warning, ali trebalo bi da radi
-        "message": message
+        "leading_two_octets": base64.b64encode(timestamp[:2]).decode('utf-8'),
+        "message_digest": base64.b64encode(signature).decode('utf-8'),
+        "message": base64.b64encode(message).decode('utf-8')
     }
 
     return json.dumps(signature_and_message).encode('utf-8')
 
 def verify_signature(message: bytes, public_key: RSAPublicKey) -> bool:
-    # TODO
-    pass
+    try:
+        data = json.loads(message.decode('utf-8'))
+
+        required_fields = ['timestamp', 'sender_key_id', 'leading_two_octets', 'message_digest', 'message']
+        # does signature contain all required fields
+        if not all(field in data for field in required_fields):
+            return False
+
+        timestamp = base64.b64decode(data['timestamp'])
+        leading_two_octets = base64.b64decode(data['leading_two_octets'])
+        message_digest = base64.b64decode(data['message_digest'])
+        original_message = base64.b64decode(data['message'])
+
+        sender_key_id = data['sender_key_id']
+
+        # is sender_id ok
+        computed_key_id = public_key.public_numbers().n % (2 ** 64)
+        if sender_key_id != computed_key_id:
+            return False
+
+        # check leading two octets
+        if len(timestamp) >= 2 and leading_two_octets != timestamp[:2]:
+            return False
+
+        # data which are signed
+        data_to_verify = timestamp + original_message
+
+        # verify signature
+        public_key.verify(
+            message_digest,
+            data_to_verify,
+            padding.PKCS1v15(),
+            hashes.SHA1()
+        )
+        return True
+    except:
+        return False
--- Test.py
+++ Test.py
@@ -4,6 +4,9 @@ import PgpService
 from PublicKeyRing import PublicKeyRing
 from PrivateKeyRing import PrivateKeyRing
 from PemFiles import export_key_pair, import_key_pair
+from Authentication import sign, verify_signature
+from cryptography.hazmat.primitives.asymmetric import rsa
+
 
 """
 Test for encryption and decryption of generated private key
@@ -19,18 +22,18 @@ password1 = "1234"
 password2 = "1235"
 
 
-# Testing generating
+# 1. TEST - generating key pair
 
 pgp.generate_private_key_pair(name, email, key_size, password1)
 
 key_id = list(pgp.private_key_ring.private_key_ring.keys())[0]
 record = pgp.private_key_ring.private_key_ring[key_id]
 encrypted_private_key = record.enc_private_key
-decrypted_bytes = pgp.private_key_ring.decrypt_private_key(encrypted_private_key, password1)
+private_key = pgp.private_key_ring.decrypt_private_key(encrypted_private_key, password1)
 
 public_key = record.public_key
 
-# Testing export and import of key pair
+# 2. TEST - export and import of key pair
 
 # export_key_pair(encrypted_private_key, public_key, name, email, time(), password1)
 #
@@ -39,3 +42,16 @@ public_key = record.public_key
 #
 # import_key_pair("Marko-marko@gmail.com-keypair.pem", password2)
 # print(PrivateKeyRing().private_key_ring_by_name)
+
+
+# TEST - Signing and verifying signature
+# private_key1 = rsa.generate_private_key(
+#     public_exponent=65537,
+#     key_size=2048
+# )
+#
+# public_key1 = private_key.public_key()
+# original_message = b"Ovo je test poruka"
+# signed_data = sign(original_message, private_key1)
+# print(verify_signature(signed_data, public_key1))
+
